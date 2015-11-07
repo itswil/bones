9208eb63f8239d8c71312137adf45e09c4b46349
William Wong <Wil@Williams-MBP.default>
2015-11-07 19:02:18 +0000

    JSHint fixes

--- static/js/src/consoleLogger.js
+++ static/js/src/consoleLogger.js
@@ -1,15 +1,13 @@
-'use strict';
-
 var $ = require('../lib/jquery-2.1.4');
 
 var consoleLogger = function() {
   var init = function() {
     console.log('jQuery has been imported: ' + $);
-  }
+  };
 
   return {
     init: init,
-  }
+  };
 }();
 
 module.exports = consoleLogger;
